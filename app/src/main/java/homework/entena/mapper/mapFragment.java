9ca461dd67949c9a85f47fa764ab25082b45d9b5
Cal <calientepermanente@gmail.com>
2015-04-09 16:01:48 -0400

    Added in custom touch support and changed view to a mapview

--- app/src/main/java/homework/entena/mapper/mapFragment.java
+++ app/src/main/java/homework/entena/mapper/mapFragment.java
@@ -6,13 +6,25 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroupOverlay;
+import android.view.ViewOverlay;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.CancelableCallback;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -22,6 +34,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class mapFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap mMap;
+    private MapView mapView;
+    private EditText editLat, editLong;
+
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
 
     public mapFragment() {
@@ -36,23 +51,56 @@ public class mapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
-        SupportMapFragment mapFragment = (SupportMapFragment) ((FragmentActivity) getActivity()).getSupportFragmentManager().findFragmentById(R.id.map);
-        mMap = mapFragment.getMap();
-        Marker hamburg = mMap.addMarker(new MarkerOptions().position(HAMBURG).title("Hamburg"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-        //mMap = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        mapView = (MapView) view.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        mMap = mapView.getMap();
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        MapsInitializer.initialize(this.getActivity());
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng arg0) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getActivity(), arg0.latitude+"-"+arg0.longitude, Toast.LENGTH_SHORT).show();
+                Log.d("arg0", arg0.latitude + "-" + arg0.longitude);
+            }
+        });
+
+        editLat = (EditText) view.findViewById(R.id.editLat);
+        editLong = (EditText) view.findViewById(R.id.editLong);
+        Button goTobtn = (Button) view.findViewById(R.id.btnGoTo);
+        goTobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goTo(v);
+            }
+        });
         return view;
+    }
+
+    public void goTo(View view) {
+        if(editLat.getText().toString().length() < 1 || editLong.getText().toString().length() < 1){
+            Toast.makeText(getActivity(), "ERROR YOU MUST PROVIDE COORDINATES!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        float latit = Float.parseFloat(editLat.getText().toString());
+        float longit = Float.parseFloat(editLong.getText().toString());
+        if(latit > 90.0 || latit < -90.0){
+            Toast.makeText(getActivity(), "ERROR: Latitude has a max of 90 and a min of -90", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(longit > 180.0 || longit < -180.0){
+            Toast.makeText(getActivity(), "ERROR: Longitude has a max of 180 and a min of -180", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(latit, longit), 10);
+        mMap.animateCamera(cameraUpdate);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        /*FragmentManager fm = getChildFragmentManager();
-        Fragment fragment = (SupportMapFragment) (fm.findFragmentById(R.id.mapBox));
-        if (fragment == null) {
-            fragment = SupportMapFragment.newInstance();
-            fm.beginTransaction().replace(R.id.map_container, fragment).commit();
-        }*/
     }
 
     @Override
@@ -67,6 +115,24 @@ public class mapFragment extends Fragment implements OnMapReadyCallback {
 
     public void onMapReady(GoogleMap map) {
         mMap = map;
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-33.87365, 151.20689), 10));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-33.87365, 151.20689), 10));
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
